6705e046cd6a71a973ada6d087e5afa8429dec77
Cari Palazzolo <cari.pizza@gmail.com>
2019-01-30 12:06:01 -0800

    improve User schema, add User test

---
+++ test/models/User.test.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+require('../../lib/utils/connect')();
+const mongoose = require('mongoose');
+const { Types } = require('mongoose');
+const User = require('../../lib/models/User');
+
+describe('User', () => {
+  beforeEach(done => {
+    return mongoose.connection.dropDatabase(() => {
+      done();
+    });
+  });
+  afterAll(done => {
+    mongoose.connection.close();
+    done();
+  });
+
+  it('validates a good model', () => {
+    const user = new User({
+      username: 'booboo3000'
+    });
+    expect(user.toJSON()).toEqual({
+      _id: expect.any(Types.ObjectId),
+      username: 'booboo3000'
+    });
+  });
+
+  it('has a required username field', () => {
+    const user = new User({});
+    const errors = user.validateSync().errors;
+    expect(errors).toBeDefined();
+    expect(errors.username['message']).toEqual('Path `username` is required.');
+  });
+
+});
+
