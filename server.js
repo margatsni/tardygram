360f509a23196c9fb34fcc063ef6fc1537144b57
Cari Palazzolo <cari.pizza@gmail.com>
2019-01-31 15:40:52 -0800

    update server PORT as constant for heroku build

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 require('./lib/utils/connect')();
 const app = require('./lib/app');
 
-app.listen(7890, () => {
+const PORT = process.env.PORT || 7890;
+
+app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log('server running on port 7890...');
+  console.log(`server running on port ${PORT}...`);
 });
