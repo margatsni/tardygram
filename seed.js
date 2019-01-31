62fc5fb4242f72d54657742c9e89e7fbce1c2f12
Cari Palazzolo <cari.pizza@gmail.com>
2019-01-31 13:01:46 -0800

    fix jest/eslint warnings

--- seed.js
+++ seed.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 require('./lib/utils/connect')();
-// const { userGramData } = require('./test/seedData');
 const userGramData = require('./test/seedData');
 const mongoose = require('mongoose');
 
 userGramData({})
+  // eslint-disable-next-line no-console
   .then(() => console.log('done'))
   .finally(() => mongoose.connection.close());
 
